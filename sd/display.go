99acd7d36153dcf9fb79cdd87a7f978168dc8f8c
Oscar Cao <oscarcao777@gmail.com>
2024-10-23 00:42:08 +0100

    added ping back to indicate user who requested image generation

--- sd/display.go
+++ sd/display.go
@@ -10,6 +10,11 @@ import (
 
 // Displays generated images on Discord
 func displayImages(s *discordgo.Session, i *discordgo.InteractionCreate, allImages *Images) error {
+	_, err := s.ChannelMessageSend(i.ChannelID, i.Interaction.Member.User.Mention()+" Image Generation Complete!")
+	if err != nil {
+		log.WithError(err).Error("Failed to send ping message.")
+		return err
+	}
 	for n, imageData := range allImages.Image {
 		reader := bytes.NewReader(imageData)
 		fileName := fmt.Sprintf("image%d.png", n+1)

64c0b3baa1f0e6fbdeae6d949ba9c03b7a1fe3f2
Oscar Cao <oscarcao777@gmail.com>
2024-10-23 00:41:25 +0100

    added response interaction in addition to adjusting permission

--- commands/generate.go
+++ commands/generate.go
@@ -3,31 +3,42 @@ package commands
 import (
 	"Dreamer/sd"
 	"context"
+	"fmt"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/spf13/viper"
 )
 
 // Handles image generation requests
 func GenerateImage(ctx context.Context, s *discordgo.Session, i *discordgo.InteractionCreate) *interactionError {
-	/*
-		g, err := s.Guild(i.GuildID)
-		if err != nil {
-			return &interactionError{err: err, message: "Unable to query guild"}
+
+	g, err := s.Guild(i.GuildID)
+	if err != nil {
+		return &interactionError{err: err, message: "Unable to query guild"}
+	}
+
+	if i.Interaction.Member.User.ID != viper.GetString("master") {
+		// Checks if guild is registered
+		if _, exists := viper.GetStringMap("verified")[g.ID]; !exists {
+			return &interactionError{&PermissionDenied{}, "Permission Denied"}
 		}
+	}
 
-			// Checks if guild is registered
-			if value, exists := viper.GetStringMap("verified")[g.ID]; exists {
-				verifiedStatus := value.(bool)
-				if !verifiedStatus {
-					return &interactionError{&PermissionDenied{}, "Permission Denied"}
-				}
-			} else {
-				return &interactionError{&PermissionDenied{}, "Permission Denied"}
-			}
-	*/
+	resp := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Flags:   1 << 6, // Whisper Flag
+			Content: "**Image Generation in Process! Please Wait!**",
+		},
+	}
+
+	if err := s.InteractionRespond(i.Interaction, resp); err != nil {
+		return &interactionError{err: err, message: "Couldn't reply to interaction."}
+	}
+
 	prompt := i.ApplicationCommandData().Options[0].StringValue()
-
-	err := sd.StableDiffusionInit(s, i, prompt)
+	fmt.Println(prompt)
+	err = sd.StableDiffusionInit(s, i, prompt)
 	if err != nil {
 		return &interactionError{err: err, message: "Error occured trying to generate image."}
 	}
